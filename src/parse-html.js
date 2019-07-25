d3d0a80e10df4bef55dd5bae3ef76714828c6f49
Jeff Blasius <jeff@prolegis.com>
2019-07-25 16:45:31 -0700

    change content type to text over html

--- src/parse-html.js
+++ src/parse-html.js
@@ -5,7 +5,7 @@ import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 const parseHtml = async ({ body }, context, cb) => {
   const { url, html } = JSON.parse(body);
 
-  const result = await Mercury.parse(url, { html });
+  const result = await Mercury.parse(url, { contentType: 'text', html });
 
   return cb(
     null,
